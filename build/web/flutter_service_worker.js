04eb49a458f0566027e25ed79e9debec218ffe14
jecastanosm <jecastanosm@concredito.com.mx>
2025-02-17 19:18:09 -0700

    buenos cambios

---
+++ build/web/flutter_service_worker.js
@@ -0,0 +1,214 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"flutter_bootstrap.js": "21e9ebfe602783d6f5fe21449facaa47",
+"version.json": "36c5a3472250c3f38a1c14fc6c9c2ba8",
+"index.html": "acf71885c0eabc0bb76730cf0ab0a1ea",
+"/": "acf71885c0eabc0bb76730cf0ab0a1ea",
+"main.dart.js": "c2b6afa94ba2f2ad04a957a6ca54787c",
+"flutter.js": "76f08d47ff9f5715220992f993002504",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "db0238e94f3b6cf7562ea6a5262bcf8d",
+"assets/AssetManifest.json": "c681e96780e6d9f6b5824a94252fd173",
+"assets/NOTICES": "9f6ab3ccb38716545120bd1431d1e2f5",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/AssetManifest.bin.json": "25336e268a6b06edc720bd9012ad52d5",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"assets/AssetManifest.bin": "e6b50db18d44c236e76ce93fd351ffcb",
+"assets/fonts/MaterialIcons-Regular.otf": "af36b36a3b97885eb644b715bdeae80c",
+"assets/assets/spaPrincipal.webp": "2e22b7ad85c8bcea497a2a8fb618cbb5",
+"assets/assets/teamSpa.jpg": "35adca5ce836b98eb6a97946e4d8fc79",
+"assets/assets/valores.jpeg": "421c840757bf81912116aeb0198c5e24",
+"assets/assets/spaPrincipal.jpg": "3e416f46e4d4025959038efe313a52c4",
+"assets/assets/vision.jpeg": "3114ef645974bcbce91e018e8ab670bd",
+"assets/assets/mision.jpeg": "b02b8c868ffc51b4d6e7e5cd54d46222",
+"assets/assets/teamTwo.jpg": "5cf7b0610fa5391e170dd993e057e1e5",
+"canvaskit/skwasm_st.js": "d1326ceef381ad382ab492ba5d96f04d",
+"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
+"canvaskit/skwasm.js.symbols": "80806576fa1056b43dd6d0b445b4b6f7",
+"canvaskit/canvaskit.js.symbols": "68eb703b9a609baef8ee0e413b442f33",
+"canvaskit/skwasm.wasm": "f0dfd99007f989368db17c9abeed5a49",
+"canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
+"canvaskit/chromium/canvaskit.js": "ba4a8ae1a65ff3ad81c6818fd47e348b",
+"canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
+"canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
+"canvaskit/canvaskit.js": "6cfe36b4647fbfa15683e09e7dd366bc",
+"canvaskit/canvaskit.wasm": "efeeba7dcc952dae57870d4df3111fad",
+"canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,308 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage();
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+              
+                  SizedBox(height: 40,),
+                  Container(
+                    width: width,
+                    height: 120,
+                    child: Image.asset("assets/teamTwo.jpg",fit: BoxFit.fitWidth,),
+                  ),
+                  SizedBox(height: 100,),
+                  Text("Te ofrecemos siempre", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),),
+                  Text("las técnicas mas avanzadas", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600, color: Colors.green)),
+                  Text("para conseguir", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600)),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      color: Colors.grey,
+                      width: width,
+                      height: 60,
+                      child: Center(
+                        child: Text("RESULTADOS INCREÍBLES", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w700),),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                        width: width,
+                        height: 400,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Misión", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700, color: Colors.white)),
+                            Text("Nuestra Mision", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400, color: Colors.white)),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(1),
+                          image:  const DecorationImage(
+                            image: AssetImage('assets/mision.jpeg'),
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                        color: Colors.blueGrey,
+                        width: width,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text("Somos una clínica integral de Estética Facial y corporal dedicada a mejorar y mantener la belleza del rostro y cuerpo buscando la unificación del concepto belleza, mediante productos de alta calidad en combinación con aparatología de ultima generación y lo ultimo en técnicas manuales lo que hace de Medico Estética un espacio único de bienestar.",
+                                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white,), textAlign: TextAlign.center,),
+                            ),
+                          ],
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                        width: width,
+                        height: 400,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Visión", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700, color: Colors.white)),
+                            Text("Nuestra Visión", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400, color: Colors.white)),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(1),
+                          image:  const DecorationImage(
+                            image: AssetImage('assets/vision.jpeg'),
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                        color: Colors.blueGrey,
+                        width: width,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text("Para el año 2026 Medico Estética se proyectara con un aumento gradual y significativo de sus clientes en cuanto a convenios empresariales y alianzas estratégicas con reconocidos cirujanos estéticos y demás profesionales de la salud y belleza corporal, Un establecimiento que será reconocido a nivel Nacional como un ejemplo de organización, servicio y compromiso presentes en cada uno de los servicios que brindamos al cliente, teniendo como prioridad ofrecer un servicio profesional y mostrar resultados.",
+                                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white,), textAlign: TextAlign.center,),
+                            ),
+                          ],
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                        width: width,
+                        height: 400,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Valores", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700, color: Colors.white)),
+                            Text("Nuestros Valores", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400, color: Colors.white)),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(1),
+                          image:  const DecorationImage(
+                            image: AssetImage('assets/valores.jpeg'),
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                        color: Colors.blueGrey,
+                        width: width,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("•Honestidad: Ser transparentes y sinceros con los pacientes",
+                                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white,),),
+                              Text("•Respeto: Tratar a los pacientes con respeto",
+                                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white,),),
+                              Text("•Compromiso: Dedicarse a los pacientes y mejorar su calidad de vida",
+                                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white,),),
+                              Text("•Profesionalismo: Dar lo mejor de si en cada caso y durante todo el proceso",
+                                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white,),),
+                              Text("•Atención Personalizada: Brindar una atención abierta y sincera para que los pacientes se sientan únicos",
+                                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white,),),
+                            ],
+                          ),
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Text("DÉJATE ASESORAR POR PROFESIONALES", textAlign: TextAlign.center,style: TextStyle(fontSize: 50, fontWeight: FontWeight.w600),),
+                  SizedBox(height: 20,),
+                  Text("+52 64-41-99-22-04", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),),
+                  SizedBox(height: 15,),
+                  Text("info@medicoestetica.com", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400, color: Colors.green),),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              enabledBorder: const OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                              ),
+                              hintText: "Nombre *"
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        TextField(
+                          decoration: InputDecoration(
+                              enabledBorder: const OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                              ),
+                              hintText: "Email *"
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        TextField(
+                          keyboardType: TextInputType.multiline,
+                          minLines: 3,
+                          maxLines: 10,
+                          decoration: InputDecoration(
+                              enabledBorder: const OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                              ),
+                              hintText: "Escribe aqui tu mensaje o consulta..."
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            Container(
+                              height: 10,
+                              width: 10,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey)
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                            Text("Acepto las condiciones de uso y politíca.")
+                          ],
+                        ),
+                        SizedBox(height: 25,),
+                        SizedBox(
+                          width: width - 16,
+                          height: 50,
+                          child: ElevatedButton(onPressed: (){},
+                            child: Text("Enviar", style: TextStyle(color: Colors.white,),),
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                                textStyle: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                        SizedBox(height: 50,),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+                width: width,
+                height: 180,
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Medico Estetica", style: TextStyle(fontSize: 40),),
+                          Text("---------------- Medical Aesthetic Expertiese", style: TextStyle(fontSize: 15),),
+                        ],
+                      ),
+                      Spacer(),
+                      Icon(Icons.phone_in_talk),
+                      SizedBox(width: 20,),
+                      Icon(Icons.telegram),
+                      SizedBox(width: 20,),
+                      Icon(Icons.facebook)
+                    ],
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(1),
+                  image:  const DecorationImage(
+                    image: AssetImage('assets/spaPrincipal.webp'),
+                    fit: BoxFit.cover,
+                  ),
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
